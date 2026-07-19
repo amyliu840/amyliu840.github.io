32301c0ce3b69bc18e77b93dfea2838c352e6cf0
amyliu840 <13980384+amyliu840@user.noreply.gitee.com>
2026-07-20 00:28:22 +0800

    fix deploy error: add esbuild

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://amyliu840.github.io"
+  base: "https://amyliu840.github.io",
+  build: {
+    cssMinify: 'esbuild' // 默认通常是 esbuild，但如果显式设置了 lightningcss，改回 esbuild 可避免严格语法报错
+  }
 })
